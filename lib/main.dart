0ba14eb985d0ee727aeeca76b92b10008d628fe9
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2022-01-04 20:24:56 +0500

    Updated: Main to welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Screens/login.dart';
+import 'package:flutter_app/Screens/Welcome.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Start());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.green),
         home: Scaffold(
           appBar: AppBar(title: Text("Booking App")),
@@ -13,5 +16,31 @@ class MyApp extends StatelessWidget {
             child: Text("Hello Usman"),
           ),
         ));
+  }
+}
+
+class Start extends StatelessWidget {
+  const Start({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Welcome",
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Login Signup UI",
+      home: LoginSignupScreen(),
+    );
   }
 }
