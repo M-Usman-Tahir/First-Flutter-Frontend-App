eeef591bb1f4729529b8dc903027a655a11c163d
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2022-01-04 20:23:15 +0500

    Added: Welcome Screen

---
+++ lib/Screens/Welcome.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/Screens/login.dart';
+import 'package:flutter_app/Screens/Monkey.dart';
+
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _Welcome createState() => _Welcome();
+}
+
+class _Welcome extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Center(
+        child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("Assets/bg1.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: Stack(children: <Widget>[
+                // Stroked text as border.
+                Center(
+                    child: Text(
+                  'Welcome',
+                  style: TextStyle(
+                    fontSize: 80,
+                    fontStyle: FontStyle.italic,
+                    foreground: Paint()
+                      ..style = PaintingStyle.stroke
+                      ..strokeWidth = 6
+                      ..color = Colors.yellow,
+                  ),
+                )),
+                // Solid text as fill.
+                Center(
+                    child: Text(
+                  'Welcome',
+                  style: TextStyle(
+                    fontSize: 80,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
+                  ),
+                )),
+                Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: TextButton(
+                          child: Text(
+                            'Continue',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          style: TextButton.styleFrom(
+                            primary: Colors.black,
+                            backgroundColor: Colors.blue[200],
+                            onSurface: Colors.grey,
+                            // padding: Edge//EdgeInsets.all(30),
+                          ),
+                          onPressed: () {
+                            // print('Pressed');
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MonkeyScreen()));
+                          },
+                        )))
+              ]),
+            )), /* add child content here */
+      ),
+    ));
+  }
+}
