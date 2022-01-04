750acf490b25845376043f549cf452cab3dc2bba
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2022-01-04 20:22:59 +0500

    Added: Sample template (incomplete)

---
+++ lib/Screens/login.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/static/staticvars.dart';
+
+class LoginSignupScreen extends StatefulWidget {
+  @override
+  _LoginSignupScreenState createState() => _LoginSignupScreenState();
+}
+
+class _LoginSignupScreenState extends State<LoginSignupScreen> {
+  bool isSignupScreen = true;
+  bool isMale = true;
+  bool isRememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: StaticVars.backgroundColor,
+        body: Stack(children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/background.jpg"),
+                      fit: BoxFit.fill)),
+              child: Container(
+                padding: EdgeInsets.only(top: 90, left: 20),
+                color: Color(0xFF3b5999).withOpacity(.85),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                          text: "Welcome to",
+                          style: TextStyle(
+                            fontSize: 25,
+                            letterSpacing: 2,
+                            color: Colors.yellow[700],
+                          ),
+                          children: [
+                            TextSpan(
+                              text: isSignupScreen ? " Rizona," : " Back,",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.yellow[700],
+                              ),
+                            )
+                          ]),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      isSignupScreen
+                          ? "Signup to Continue"
+                          : "Signin to Continue",
+                      style: TextStyle(
+                        letterSpacing: 1,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ]));
+  }
+}
